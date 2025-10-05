2d6c33c17cc63a445c2bf44257ba46a7f3870079
KaaNyaa <114100504+KaaNyaa@users.noreply.github.com>
2025-10-05 14:38:47 -0400

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Micheletti_Company.Models; 
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Micheletti_Company.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Company> Company { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
