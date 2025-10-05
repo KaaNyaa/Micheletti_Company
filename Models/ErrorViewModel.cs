ede17f28d18f77bd314ac5d94229d279a5b45867
KaaNyaa <114100504+KaaNyaa@users.noreply.github.com>
2025-10-05 14:39:26 -0400

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Micheletti_Company.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
