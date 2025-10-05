938009a5be3183c4622e88f338cd1ef03a91a681
KaaNyaa <114100504+KaaNyaa@users.noreply.github.com>
2025-10-05 14:36:48 -0400

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Micheletti_Company.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
